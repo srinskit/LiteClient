bfe6d8cf7ceea8e89b99daa480c6e2a2ded93560
srinskit <srinagrao2007@gmail.com>
2018-03-24 15:43:17 +0530

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='LiteClient',
+    version='0.0',
+    packages=[''],
+    url='https://github.com/srinskit/LiteClient',
+    license='',
+    author='srinag',
+    author_email='srinagrao2007@gmail.com',
+    description=''
+)
